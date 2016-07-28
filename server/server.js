997e7a5d11d862f4fa5b2500495ec8d6638d074f
Deniz Tetik <deniztetik31@gmail.com>
2016-07-28 12:00:39 -0400

    [fix] Change port for localhost to 8080 from 8000
    This is required for AWS EC2

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 
 /// for development ///
 const morgan = require('morgan');
