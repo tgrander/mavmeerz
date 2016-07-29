e6fd114faf0f25129659ffacee9ccb5ab5a93e6a
tgrander <treygranderson@gmail.com>
2016-07-29 16:32:51 -0400

    [doc] Added directories to src folder to create shell of Redux

---
+++ src/actions/actions.js
@@ -0,0 +1,18 @@
+//ACTION CREATORS AND CONSTANTS
+
+  //API Requests
+    //1. Request expenses
+    //2. Receive expenses
+    //3. Request failed
+    //4. Send CSV file
+
+  //User Interactions
+    //1. Upload file (upload button clicked)
+    //2. Add category to individual expense
+
+import fetch from 'isomorphic-fetch'
+
+export const REQUEST_EXPENSES = 'REQUEST_EXPENSES';
+export const RECIEVE_EXPENSES = 'RECIEVE_EXPENSES';
+export const REQUEST_FAIL = 'REQUEST_FAIL';
+export const SEND_FILE = 'SEND_FILE'
