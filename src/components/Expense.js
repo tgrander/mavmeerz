4d4c4871da013ad90c2dcfb8c0c3f1b99aa026b9
tgrander <treygranderson@gmail.com>
2016-07-30 16:02:01 -0400

    [doc] Add Expense.js component

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -1,7 +1,31 @@
 import React from 'react'
 
-let Expense = ({expense}) => (
-  <div>
-    
-  </div>
-)
+//props
+  //expense object
+    //{id: 1, date: integer, text: string, amount: integer, category: string}
+  //submitCategory() callback
+    //dispatches ADD_CATEGORY action
+let Expense = ({exp, handleCategorySubmit}) => {
+  //component that returns a single expense item
+  //each expense item contains a date, description text, amount, and a category
+
+  return (
+    <tr>
+      <td className="date">{exp.date}</td>
+      <td className="description">{exp.text}</td>
+      <td className="category">
+        <input
+          className="category-entry"
+          type="text" 
+          placeholder="Enter Category"
+          onSubmit={e => {
+            e.preventDefault()
+            handleCategorySubmit(e)
+          }}
+        >
+        </input>
+      </td>
+      <td className="amount">{exp.amount}</td>
+    </tr>
+  )
+}
