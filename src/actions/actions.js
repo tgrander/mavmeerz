ab2440d393e9fe445f36b5e475b41a68a3fb575c
tgrander <treygranderson@gmail.com>
2016-07-30 19:17:14 -0400

    [doc] Add basic outline of Redux to app, including store, actions, components, and reducers

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -16,33 +16,44 @@ export const REQUEST_EXPENSES = 'REQUEST_EXPENSES';
 export const RECIEVE_EXPENSES = 'RECEIVE_EXPENSES';
 export const REQUEST_FAIL = 'REQUEST_FAIL';
 export const UPLOAD_FILE = 'UPLOAD_FILE';
-export const ADD_CATEGORY = 'ADD_CATEGORY';
+// export const ADD_CATEGORY = 'ADD_CATEGORY';
+// export const RECEIVE_UPDATED = 'RECEIVE_UPDATED'
 
-export function addCategory(id, category){
-  return {
-    type: ADD_CATEGORY,
-    id,
-    category
-  }
-}
-
+/*
+  @returns object with action type
+*/
 function requestExpenses(){
   return {
     type: REQUEST_EXPENSES
   }
 }
 
+/*
+  @returns object with action type & array of
+  (the return value of expenses needs to be reformatted to look like this:
+    expenses: {
+      1: {
+        ...
+      }
+    }
+  )
+*/
 function receiveExpenses(json){
   return {
-    type: RECIEVE_EXPENSES,
-    expenses: json.data.children.map(child => child.data),
-    receivedAt: Date.now()
+    type: RECEIVE_EXPENSES,
+    expenses: json.data.children.map(child => child.data)
   }
 }
 
+//thunk action creator
 export function fetchExpenses(){
   return function(dispatch){
+    //first dispatch:
+      //app state is updated to inform that the API call is starting
     dispatch(requestExpenses())
-    return fetch()
+    return fetch('/')
+      .then(res => res.json())
+      .then(json => dispatch(receiveExpenses(json)))
+      .catch(err => console.error("Error sending request to server: ", err))
   }
 }
