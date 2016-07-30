61ee49ad98f70e0104007b4dad860357848ef462
Jearxj <jear.jujaroen@gmail.com>
2016-07-30 15:43:17 -0400

    working on TotalDisplay component. Committing before rebasing

---
+++ src/components/TotalDisplay.js
@@ -0,0 +1,13 @@
+'esversion: 6';
+
+import React, { PropTypes } from 'react';
+
+const Total = ({ integer }) => (
+  <div> Total: { integer } </div>
+)
+
+Todo.propTypes = {
+  integer:
+}
+
+export default TotalDisplay;
