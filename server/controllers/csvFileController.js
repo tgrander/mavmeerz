18fb06a8aec4b2a62efc86dca0fa5ab6f570f9d9
kaziprime@gmail.com <nazreenkazi@Nazreens-MacBook-Pro.local>
2016-08-01 16:28:22 -0400

    [feat] Add new database function to csv controller

--- server/controllers/csvFileController.js
+++ server/controllers/csvFileController.js
@@ -2,6 +2,10 @@
 const File = require('../models/csvFile.js')
 
 
-exports.addExpense = (csvTitle, callback) => {
+exports.addFile = (csvTitle, callback) => {
   new File({csvTitle: csvTitle}).save().then(callback);
+};
+
+exports.getFileId = (csvTitle,callback) => {
+  new File().query('where','csvTitle','=','fileName').fetch().then((data) => data.attributes.id)
 };
