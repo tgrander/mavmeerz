147bd15b2095f36e57e96892d8a09d380bd5ee2b
Jearxj <jear.jujaroen@gmail.com>
2016-07-31 20:20:41 -0400

    renamed TotalDisplay to Total in components, created store and added Provider in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducer from './reducers'
 import App from './App';
 import './index.css';
 import 'babel-polyfill'
 
+let store = createStore(reducer)
+
 ReactDOM.render(
+  <Provider store={store}>
   <App />,
+  </Provider>,
   document.getElementById('root')
 );
