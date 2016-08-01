548100d1e962e12274262633ccb90ddef7eca871
Jearxj <jear.jujaroen@gmail.com>
2016-08-01 16:57:19 -0400

    fixed error in store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,7 @@
 import {createStore, applyMiddleware} from 'redux'
 import ReduxPromise from 'redux-promise'
 import createLogger from 'redux-logger'
-<<<<<<< e2ba7a4a1e8cd22dbafd50bc573de965fd8dc51a
 import rootReducer from '../reducers/reducers.js'
-=======
-import rootReducer from './reducers'
->>>>>>> added some console.log for deugging purposes
 
 const loggerMiddleware = createLogger()
 console.log('outside', rootReducer);
