35d5791cc073982bd8482d436410093670dc0873
Jearxj <jear.jujaroen@gmail.com>
2016-08-01 20:49:58 -0400

    fixed merge conflict

--- src/App.js
+++ src/App.js
@@ -3,28 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 import AsyncApp from './containers/AsyncApp'
 import {Provider} from 'react-redux';
-import configureStore from './store/configureStore.js'
 import DropzoneContainer from './components/Upload.js'
-
-const store = configureStore()
 
 
 class App extends Component {
   render() {
     return (
-      <Provider store={configureStore}>
-        <div className="App">
-          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <DropzoneContainer />
-          </div>
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <DropzoneContainer />
         </div>
-<<<<<<< 773e2f54062ba61d1a1798ca47a5a5541340d6b7
         <AsyncApp></AsyncApp>
-      </Provider>
-=======
-      // </Provider>
->>>>>>> fixing merge conflict
+      </div>
     );
   }
 }
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,9 +4,9 @@ import createLogger from 'redux-logger'
 import rootReducer from '../reducers/reducers.js'
 
 const loggerMiddleware = createLogger()
-console.log('outside', rootReducer);
+
 export default function configureStore(initialState) {
-  console.log('inside', rootReducer);
+
   return createStore(
     rootReducer,
     initialState,
