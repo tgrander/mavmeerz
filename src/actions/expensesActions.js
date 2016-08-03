4efb1d98d9ac669b28d1c1200cffc29efce222cb
tgrander <treygranderson@gmail.com>
2016-08-03 18:00:17 -0400

    [refactor] Combine expenses and upload reducers to create a seamless Async flow in application

--- src/actions/expensesActions.js
+++ src/actions/expensesActions.js
@@ -1,7 +1,18 @@
 import Axios from 'axios'
+/*
+Axios + redux-promise http request to server for expenses
+  -> When redux-promise is used (it is imported in configureStore.js),
+  .then / .catch are automatically taken care of. Becasue the return object
+  includes 'payload' property with a value of the Axios request, redux-promise
+  knows to wait to return object until the status of the Axios request changes
+  from 'pending' to 'success'
+*/
 
 export const REQUEST_EXPENSES = 'REQUEST_EXPENSES'
 export const RECEIVE_EXPENSES = 'RECEIVE_EXPENSES'
+export const UPLOAD_REQUEST = 'UPLOAD_REQUEST';
+export const UPLOAD_SUCCESS = 'UPLOAD_SUCCESS'
+export const UPLOAD_FAILURE = 'UPLOAD_FAILURE';
 
 //ACTION CREATORS FOR FETCHING AND RECEIVING EXPENSES FROM SERVER
 
@@ -11,32 +22,55 @@ Notifies state that expenses have been requested -> signal 'load feature'
 */
 function requestExpenses(){
   return {
-    type: REQUEST_EXPENSES
+    type: REQUEST_EXPENSES,
+    isFetching: true
   }
 }
-
+//Action creator
+function uploadRequest(){
+  return {
+    type: UPLOAD_REQUEST,
+    isFetching: true
+  };
+}
+//Action creator
+function receiveExpenses(expenses){
+  return {
+    type: RECEIVE_EXPENSES,
+    isFetching: false,
+    expenses: expenses
+  }
+}
+//Action creator
+function uploadSuccess(response){
+  return {
+    type: UPLOAD_SUCCESS,
+    isFetching: false,
+    expenses: response
+  }
+}
 /*
 Async Action creator
 Create async flow that notifies state of different stages of fetching expenses
 as the expenses are requested and received
 */
 export function fetchExpenses(){
-  //first thing is to trigger 'isFetching' to true in state
-  requestExpenses()
-  /*
-  Axios + redux-promise http request to server for expenses
-    -> When redux-promise is used (it is imported in configureStore.js),
-    .then / .catch are automatically taken care of. Becasue the return object
-    includes 'payload' property with a value of the Axios request, redux-promise
-    knows to wait to return object until the status of the Axios request changes
-    from 'pending' to 'success'
-  */
-  const request = Axios.get("/v1/api/expenses/")
-  //changes 'isFetching' to false upon success of Axios request
-  //and populates state 'expenses' with resonse data from server
-  return {
-    type: RECEIVE_EXPENSES,
-    isFetching: false,
-    payload: request
+  return dispatch => {
+    dispatch(requestExpenses())
+    return Axios.get("/v1/api/expenses/")
+      .then(res => dispatch(receiveExpenses(res.data)))
+      .catch(err => console.error(err))
+  }
+}
+export function uploadCSV(csv){
+  return dispatch => {
+    dispatch(uploadRequest())
+    return Axios({
+      method: 'POST',
+      url: '/v1/api/expenses',
+      data: csv
+    })
+    .then(res => dispatch(uploadSuccess(res.data)))
+    .catch(err => console.error(err))
   }
 }
