50c606bd465e7e714d278e8c07ee048337a76224
tgrander <treygranderson@gmail.com>
2016-08-03 10:35:19 -0400

    [feat] Add async flow functions to auto refresh page when CSV uploads

--- src/actions/expensesActions.js
+++ src/actions/expensesActions.js
@@ -9,42 +9,21 @@ Action creator
 @returns object with action type
 */
 
-const exampleData = [
-  {
-    id: 1,
-    date: 1470085785371,
-    description: "Whole Foods Market",
-    amount: 69,
-    category: "other"
-  },
-  {
-    id: 2,
-    date: 1470085785371,
-    description: "Sephora",
-    amount: 39,
-    category: "other"
-  },
-  {
-    id: 3,
-    date: 1470085785371,
-    description: "Restaurant in West Village",
-    amount: 96,
-    category: "other"
-  },
-  {
-    id: 4,
-    date: 1470085785371,
-    description: "Pizza Shop",
-    amount: 5.50,
-    category: "other"
+function requestExpenses(){
+  return {
+    type: REQUEST_EXPENSES
   }
-]
-
-
+}
+function receiveExpenses(){
+  return {
+    type: RECEIVE_EXPENSES
+  }
+}
 export function fetchExpenses(){
   const request = Axios.get("/v1/api/expenses/")
-  console.log(request);
-  // const request = exampleData
+  .then(res => )
+  .catch(err => console.error(err))
+
   return {
     type: REQUEST_EXPENSES,
     payload: request
