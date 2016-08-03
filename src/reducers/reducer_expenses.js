3031d05d76e1918be8343a5111a2efc798fb676b
tgrander <treygranderson@gmail.com>
2016-08-03 14:16:11 -0400

    [feat] Calculate Total of all expenses and render to page

--- src/reducers/reducer_expenses.js
+++ src/reducers/reducer_expenses.js
@@ -1,39 +1,9 @@
 'esversion: 6';
 
 import {
-  REQUEST_EXPENSES
+  REQUEST_EXPENSES,
+  RECEIVE_EXPENSES
 } from '../actions/expensesActions.js';
-
-const exampleData = [
-  {
-    id: 1,
-    date: 1470085785371,
-    description: "Whole Foods Market",
-    amount: 69,
-    category: "other"
-  },
-  {
-    id: 2,
-    date: 1470085785371,
-    description: "Sephora",
-    amount: 39,
-    category: "other"
-  },
-  {
-    id: 3,
-    date: 1470085785371,
-    description: "Restaurant in West Village",
-    amount: 96,
-    category: "other"
-  },
-  {
-    id: 4,
-    date: 1470085785371,
-    description: "Pizza Shop",
-    amount: 5.50,
-    category: "other"
-  }
-]
 
 const INITIAL_STATE = {expenses: [], isFetching: false}
 
@@ -41,6 +11,12 @@ export default function expenses(state=INITIAL_STATE, action){
   switch (action.type) {
     case REQUEST_EXPENSES:
       return Object.assign({}, state, {
+        isFetching: action.isFetching
+      })
+      break;
+    case RECEIVE_EXPENSES:
+      return Object.assign({}, state, {
+        isFetching: action.isFetching,
         expenses: action.payload.data
       })
       break;
