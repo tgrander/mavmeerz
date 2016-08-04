b4ff53e9394e7f81a4e83b7e49e7e65e1cfbd478
tgrander <treygranderson@gmail.com>
2016-08-04 10:32:50 -0400

    [fix] merge conflict

---
+++ src/containers/UploadApp.js
@@ -0,0 +1,18 @@
+/*
+Upload App container (smart component) in charge of connecting React to the
+store/state and transforming the state in a way that can be used by the
+UploadButton component.
+  -Renders UploadButton
+*/
+
+import React, { Component } from 'react'
+
+export default class UploadApp extends Component {
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+
+  }
+}
