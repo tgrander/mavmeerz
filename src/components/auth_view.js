cae018a294465d53e708fe708db765bf029c1cd4
tgrander <treygranderson@gmail.com>
2016-08-04 20:12:21 -0400

    [feat] react router rough draft 2

--- src/components/auth_view.js
+++ src/components/auth_view.js
@@ -18,14 +18,14 @@ class AuthView extends Component {
   render() {
     return (
       <div className="auth-view">
-        <h2 className="auth-view-logo"> className="heading">ZENMO</h2>
+        <h2 className="auth-view-logo">ZENMO</h2>
         <p className="auth-view-slogan">Attain Financial Nirvana</p>
-        <br/><br/>
-        <p className="auth-view-p">Get Started On Your Journey to Mindful Budgeting</p>
+        <br/>
+        <p className="auth-view-p">Begin Your Journey to Mindful Budgeting</p>
         <Link to="/signup" className="btn hvr-bounce-to-left">SIGNUP</Link>
         <br/>
         <p className="auth-view-p">Or Continue To Collect Budget Karma</p>
-        <Link to="/login" className="btn hvr-bounce-to-left">LOGIN/</Link>
+        <Link to="/login" className="btn hvr-bounce-to-left">LOGIN</Link>
       </div>
     )
   }
--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,6 +1,5 @@
 import React, {Component, PropTypes} from 'react'
 import {connect} from 'react-redux'
-import {fetchGroups} from '../actions/dashboardActions'
 import {Link} from 'react-router'
 import ReactAnimate from 'react-addons-css-transition-group'
 
@@ -46,7 +45,7 @@ class Dashboard extends Component {
       </div>
     )
   }
-  
+
 }
 
 export default connect(
