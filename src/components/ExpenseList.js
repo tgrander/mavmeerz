3f964e62bf771f24763fadedfd5f1549f9671354
tgrander <treygranderson@gmail.com>
2016-08-06 15:13:25 -0400

    [feat] Table displays expenses

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,32 +1,32 @@
 import React from 'react'
-import Expense from './Expense.js'
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+// import Expense from './Expense.js'
 import Upload from '../containers/UploadApp'
 
 let ExpenseList = ({expenses}) => {
-
+  console.log('Expenselist expenses are present: ', expenses.length>0);
   if (expenses.length>0) {
     //map function that returns and array of Expense Component, each with its
     //own expese object
-    let expenseList = expenses.map(exp =>
-      <Expense
-        key={exp.id}
-        exp={exp}
-      />
-    )
-
-
+    // let expenseList = expenses.map(exp =>
+    //   <Expense
+    //     key={exp.id}
+    //     exp={exp}
+    //   />
+    // )
 
     return (
       <div className="transactions">
         <h3>TRANSACTIONS</h3>
-        <BootstrapTable data={ this.props.expenses } insertRow={ true } striped={ true } hover={ true } deleteRow={ true } columnFilter={ true } exportCSV={ true }
+        <BootstrapTable data={ expenses } insertRow={ true } striped={ true } hover={ true } deleteRow={ true } columnFilter={ true } exportCSV={ true }
           selectRow={{mode: 'checkbox', clickToSelect: true}}
         >
+          <TableHeaderColumn dataField='id' isKey={ true }>ID</TableHeaderColumn>
           <TableHeaderColumn dataField='date' editable={ { type: 'textarea' } }>Date</TableHeaderColumn>
           <TableHeaderColumn dataField='description' editable={ { type: 'textarea' } }>Description</TableHeaderColumn>
           <TableHeaderColumn dataField='category' editable={ { type: 'checkbox', options: { values: 'Y:N' } } }>Category</TableHeaderColumn>
           <TableHeaderColumn dataField='amount' editable={ { type: 'checkbox', options: { values: 'Y:N' } } }>Amount</TableHeaderColumn>
-          <TableHeaderColumn dataField='essential' editable={ { type: 'checkbox', options: { values: 'Y:N' } } }>Amount</TableHeaderColumn>
+        
         </BootstrapTable>
       </div>
     )
