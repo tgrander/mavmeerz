d5ea98eac7cf759ae4f559e5b6c092e8f01dae3e
kaziprime@gmail.com <nazreenkazi@Nazreens-MacBook-Pro.local>
2016-08-12 12:36:38 -0400

    [feat] Add getSubCategoryId controller function in subCategoryController

--- server/controllers/subCategoryController.js
+++ server/controllers/subCategoryController.js
@@ -5,6 +5,14 @@ exports.addSubCategory = (sCat) => {
   new subCategory({sub_category: sCat}).save();
 };
 
+exports.getSubCategoryId = (cat) => {
+  return new Promise((resolve,reject) => {
+    new subCategory().query("where", "sub_category", "=", cat).fetch().then((data) => {
+      resolve(data.attributes.id)
+    })
+  });
+};
+
 exports.checkSubCategoryTable = () => {
   return new Promise((resolve,reject) => {
       new subCategory().fetch().then((data) => resolve(data));
