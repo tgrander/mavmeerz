0ae2c0e75e23b04dfcaac2df737c0f55a392e696
Jearxj <jear.jujaroen@gmail.com>
2016-08-13 16:24:38 -0400

    [feat] date range picker modal hides and shows on click and on backdrop

--- src/components/Datepicker.js
+++ src/components/Datepicker.js
@@ -1,24 +1,35 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import '../css/datepicker.css'
 import { MultiMonthView } from 'react-date-picker'
+import Dropdown from './Dropdown'
+import { Modal } from 'react-bootstrap';
 
-const DatePicker = () => {
+export class DatePicker extends Component {
+  constructor(props) {
+    super(props)
 
-  console.log('DatePicker!');
-  const now = Date.now()
+  }
+// const DatePicker = () => {
 
-  const hour = 1000 * 60 * 60
-  const day = 24 * hour
+  render() {
+    console.log('DatePicker!');
+    return (
+      <div>
+        <MultiMonthView defaultRange={[]}/>
 
-  const in3days = now + day * 3
-
-  return (
-    <div>
-      <MultiMonthView defaultRange={[now, in3days]}/>
-    </div>
-  )
+      </div>
+    )
+  }
 }
-
+// }
 
 
 export default DatePicker
+
+// const now = Date.now()
+//
+// const hour = 1000 * 60 * 60
+// const day = 24 * hour
+//
+// const in3days = now + day * 3
+// <input type="button" onClick={this.props.toggle}/>
