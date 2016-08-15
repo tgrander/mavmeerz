4701ba21c8d3f421cd508b4e82e29e6696d516c3
Jearxj <jear.jujaroen@gmail.com>
2016-08-15 11:34:55 -0400

    [cleanup] remove dead code

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -21,37 +21,5 @@ class DatePicker extends Component {
 }
 
 export default DatePicker
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-import React, {Component} from 'react'
-import '../css/datepicker.css'
-import { MultiMonthView } from 'react-date-picker'
-import Dropdown from './Dropdown'
-import { Modal } from 'react-bootstrap';
 
-export class DatePicker extends Component {
-  constructor(props) {
-    super(props)
-
-  }
-
-  render() {
-    console.log('DatePicker!');
-    return (
-      <div>
-        <MultiMonthView defaultRange={[]}/>
-
-      </div>
-    )
-  }
-}
-
-export default DatePicker
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-
-// const now = Date.now()
-//
-// const hour = 1000 * 60 * 60
-// const day = 24 * hour
-//
-// const in3days = now + day * 3
 // <input type="button" onClick={this.props.toggle}/>
