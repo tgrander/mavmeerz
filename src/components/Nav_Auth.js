841dbd20785f6da59a7534400fda1647a0cdd0bb
tgrander <treygranderson@gmail.com>
2016-08-24 11:01:28 -0400

    [feat] Style login page

--- src/components/Nav_Auth.js
+++ src/components/Nav_Auth.js
@@ -6,8 +6,6 @@ const Nav_Auth = () => (
   <nav class='fixed-nav-bar'>
     <ul className="nav">
       <li><IndexLink to="/">ZENMO</IndexLink></li>
-      <li className="nav-item"><Link to='/' classname='nav-logo'>About</Link></li>
-      <li className="nav-item"><Link to='/' classname='nav-logo'>Dev Team</Link></li>
     </ul>
   </nav>
 )
--- src/components/login_form.js
+++ src/components/login_form.js
@@ -4,7 +4,8 @@ import {reduxForm} from 'redux-form'
 import { Link } from 'react-router'
 import { login } from '../actions/authActions'
 
-import Nav_Auth from './Nav_Auth'
+import '../css/login.css'
+import '../css/particles.css'
 
 class LoginForm extends Component {
   static contextTypes = {
@@ -27,25 +28,33 @@ class LoginForm extends Component {
   render() {
     const {fields:{email, password}, handleSubmit} = this.props;
     return (
-      <div>
-        <Nav_Auth/>
-        <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
-          <p className="heading">Login</p>
-            <div>
-              <input type="text" {...email} placeholder='name' className="biginput"/>
-              <div className="err-msg">{email.touched ? email.error : ''}</div>
-            </div>
 
-            <div>
-              <input type="password" {...password} placeholder='password' className="biginput"/>
-              <div className="err-msg">{password.touched ? password.error : ''}</div>
+      <div className="login-page">
+
+        <div id="login-form">
+          <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+            <p className="heading">LOGIN</p>
+              <div>
+                <input type="text" {...email} placeholder='username' className="biginput"/>
+                <div className="err-msg">{email.touched ? email.error : ''}</div>
+              </div>
+              <div>
+                <input type="password" {...password} placeholder='password' className="biginput"/>
+                <div className="err-msg">{password.touched ? password.error : ''}</div>
+              </div>
+            <div className="btn-group">
+              <button type="submit" className="submit">SUBMIT</button>
             </div>
-          <div className="btn-group">
-            <button type="submit" className="btn hvr-bounce-to-left">Submit</button>
-          </div>
-        </form>
-        <Link to="/signup" className="btn hvr-bounce-to-left">SIGNUP</Link>
+          </form>
+        </div>
+
+        <div className="auth-redirect">
+          <p>Already have an account?</p>
+          <Link to="/signup" className="redirect-link">SIGNUP</Link>
+        </div>
+
       </div>
+
     )
   }
 }
