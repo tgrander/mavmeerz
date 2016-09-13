848c674a4db376b745f66085725d7c801140edae
tgrander <treygranderson@gmail.com>
2016-09-13 13:25:04 -0400

    Delete and re-install node-modules

---
+++ src/components/MainContent.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+const MainContent = () => (
+  <main className="auth-view" role='main' class='main-content'>
+    <div className='inner above-fold'>
+
+        
+
+        <div className="logo-and-phrase">
+          <h1 className="logo" id='title'>ZENMO</h1>
+          <p className="phrase">Find Your Path To Financial Nirvana</p>
+        </div>
+
+    </div>
+  </main>
+)
+
+export default MainContent
