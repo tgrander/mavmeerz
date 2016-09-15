a1dc29ed06da0a1dc923ad6dd2dc3dd966a4c09d
tgrander <treygranderson@gmail.com>
2016-09-15 12:24:22 -0400

    align dropdown with table links

--- src/components/DashLinks.js
+++ src/components/DashLinks.js
@@ -2,8 +2,10 @@ import React from 'react'
 import { Link, IndexLink } from 'react-router';
 
 import Dropdown from './Dropdown'
+import Categories from './DropdownCategory'
 
 //css passed fown from dashboard.css
+import '../css/dropdown.css'
 
 const DashLinks = () => {
   return (
@@ -14,10 +16,18 @@ const DashLinks = () => {
         <li className="dash-link"><Link to='/budget'>BUDGET</Link></li>
       </ul>
 
-      <ul className="dash-paths">
-        <li className="dash-link"><Link to='/'>TRANSACTIONS</Link></li>
-        <li className="dash-link"><Link to='/budget'>BUDGET</Link></li>
+    <nav id="primary_nav_wrap">
+      <ul className='dash-paths'>
+        <li><a href="#">Categorize</a>
+          <Categories
+
+          />
+        </li>
+        <li className="current-menu-item"><a href="#" >Upload CSV</a></li>
+        <li className="current-menu-item"><a href="#" >Show All Expenses</a></li>
+        <li className="current-menu-item"><a href="#" >Filter By Date</a></li>
       </ul>
+    </nav>
 
     </div>
   )
